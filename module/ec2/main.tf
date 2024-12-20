ecde9831846958e04c3025b52e1cc0d1e9622fba
nishalkarthik <58972208+nishalkarthik@users.noreply.github.com>
2024-12-20 18:27:59 +0530

    Update main.tf

--- module/ec2/main.tf
+++ module/ec2/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "webservers" {
   count         = var.pubinstancecount                                        #length(aws_subnet.public-subnet.*.id)
   ami           = var.amiid
   instance_type = var.machinetype
-  subnet_id     = element(var.instance-pub-subnet,count.index)
+  subnet_id     = element(var.instance-pub-subnet,count.index)            # element(list,index)---list,it looks for the list of string [list(string)] for where we need to get the element/values and index tells which particular it needs to take from the list eg: 0,1,2.....
   key_name      = var.keyname
   vpc_security_group_ids = var.instance-vpcSG-id
   user_data = file("install_httpd.sh")
